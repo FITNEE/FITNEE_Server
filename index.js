2d1bd997b3b0161bdd469cd65c5e66069d2a6c6e
fbwogur121 <65850233+fbwogur121@users.noreply.github.com>
2023-07-21 20:15:45 +0900

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('./config/express');
-const {logger} = require('./config/winston');
-
-const port = 3000;
-express().listen(port);
-logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
