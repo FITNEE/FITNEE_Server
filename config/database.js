16fdb171e19be9b5aa4d0a5b810e7875b283bab8
fbwogur121 <65850233+fbwogur121@users.noreply.github.com>
2023-07-21 19:27:23 +0900

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+const {logger} = require('./winston');
+
+//DB 계정 입력
+const pool = mysql.createPool({
+    host: 'health-gpt.cuuueh7vuqzb.ap-northeast-2.rds.amazonaws.com',
+    user: 'root',
+    port: '3306',
+    password: 'healthgpt',
+    database: 'health-gpt'
+});
+
+module.exports = {
+    pool: pool
+};
