41c8296f40adbbf59410a84507ccbd218cd20530
ParkJunGyu26 <junju404@naver.com>
2023-07-23 16:21:14 +0900

    swagger 기본 세팅 완료(자동화는 X)

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,14 @@
-const router = require("express").Router()
-// const user = require("./user")
+// const router = require("express").Router()
+// // const user = require("./user")
 
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: 유저 추가 수정 삭제 조회
- */
-router.use("/user", user)
+// /**
+//  * @swagger
+//  * tags:
+//  *   name: Users
+//  *   description: 유저 추가 수정 삭제 조회
+//  */
+// router.use("/user", user)
 
 
 
-module.exports = router
+// module.exports = router
