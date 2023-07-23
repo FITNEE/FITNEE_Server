11bd37fd5740d65d8251e9f4aa6fbd92f247364c
헥스/박해승 <2001phs92@gmail.com>
2023-07-23 15:39:59 +0900

    change getUsers query

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -1,7 +1,7 @@
 // 모든 유저 조회
 async function selectUser(connection) {
   const selectUserListQuery = `
-                SELECT email, nickname 
+                SELECT email, userNickname 
                 FROM User;
                 `;
   const [userRows] = await connection.query(selectUserListQuery);
@@ -11,7 +11,7 @@ async function selectUser(connection) {
 // 이메일로 회원 조회
 async function selectUserEmail(connection, email) {
   const selectUserEmailQuery = `
-                SELECT email, nickname 
+                SELECT email, userNickname 
                 FROM User
                 WHERE email = ?;
                 `;
