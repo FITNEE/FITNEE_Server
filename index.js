4d64f55dcb911422d48919414833337aa1f9671b
헥스/박해승 <2001phs92@gmail.com>
2023-08-03 03:22:29 +0900

    test

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const express = require('./config/express');
 const {logger} = require('./config/winston');
-// const cookieParser = require('cookie-parser');
-// const { swaggerUi, specs } = require("./swagger/swagger");
-
-// const userRoute = require('./src/app/User/userRoute')
-
-/**
- * @path {GET} https://gpthealth.shop/
- * @description 요청 데이터 값이 없고 반환 값이 있는 GET Method
- */
 
 const port = 3000;
 express().listen(port, () => {
