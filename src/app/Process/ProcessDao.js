f87f8981411d9fbaff8cb1af0bb140389bebbfb3
ParkJunGyu26 <junju404@naver.com>
2023-08-03 09:52:39 +0900

    BeforeProcessDetail api

--- src/app/Process/ProcessDao.js
+++ src/app/Process/ProcessDao.js
@@ -1,5 +1,54 @@
 const lodash = require('lodash');
 
+// 운동 전 Detail 조회
+async function selectBeforeProcessDetail(connection, routineDetailIdx) {
+    const selectBeforeProcessDetailQuery = `
+    SELECT 'total_not_null_count' AS data_type, 
+        COUNT(*) AS data_value
+    FROM routineDetail
+    WHERE routineDetailIdx = ? 
+        AND COALESCE(rep0, rep1, rep2, rep3, rep4, rep5, rep6, rep7, rep8, rep9) IS NOT NULL
+UNION
+    SELECT 'data_weight0' AS data_type, 
+        weight0 AS data_value
+    FROM routineDetail
+    WHERE routineDetailIdx = ? 
+    LIMIT 1
+UNION
+    SELECT 'data_rep0' AS data_type, 
+        rep0 AS data_value
+    FROM routineDetail
+    WHERE routineDetailIdx = ? 
+    LIMIT 1;
+    `;
+    const [beforeProcessDetail] = await connection.query(selectBeforeProcessDetailQuery, routineDetailIdx)
+    return beforeProcessDetail
+}
+
+// 운동 중 / 세트, 무게, 횟수
+async function selectRoutineDetail(connection, routineIdx) {
+    const selectRoutineQuery = `
+                  SELECT detailIdx0, detailIdx1, detailIdx2, detailIdx3, detailIdx4, detailIdx5, detailIdx6, detailIdx7, detailIdx8, detailIdx9
+                  FROM routine
+                  WHERE status = 0 AND routineIdx = ?
+                  `;
+    const [[routine]] = await connection.query(selectRoutineQuery, routineIdx);
+    if (!routine) return routine;
+
+    for (var i=0; i<10; i++) {
+        const repColumn = `rep${i}`;
+        const weightColumn = `weight${i}`
+
+        const selectRoutineDetailQuery = `
+            SELECT ${repColumn}, ${weightColumn}
+            FROM routineDetail
+            WHERE status = 0 AND routineDetailIdx = ?
+            `;
+        const [[routineDetail]] = await connection.query(selectRoutineDetailQuery, routine['detailIdx'+String(i)]);
+    }
+    return [routineDetail]
+}
+
 // 루틴 일정 조희
 async function selectRoutineCalendar(connection, userId) {
     const selectRoutineCalendarQuery = `
@@ -39,37 +88,8 @@ async function selectRoutine(connection, routineIdx) {
     return routineContent;
 };
 
-// 루틴 삭제
-async function deleteRoutine(connection, userId, routineIdx) {
-    const deleteRoutineCalendar = `
-                  UPDATE routineCalendar
-                  SET
-                      monRoutineIdx = CASE WHEN monRoutineIdx = ? THEN 0 ELSE monRoutineIdx END,
-                      tueRoutineIdx = CASE WHEN tueRoutineIdx = ? THEN 0 ELSE tueRoutineIdx END,
-                      wedRoutineIdx = CASE WHEN wedRoutineIdx = ? THEN 0 ELSE wedRoutineIdx END,
-                      thuRoutineIdx = CASE WHEN thuRoutineIdx = ? THEN 0 ELSE thuRoutineIdx END,
-                      friRoutineIdx = CASE WHEN friRoutineIdx = ? THEN 0 ELSE friRoutineIdx END,
-                      satRoutineIdx = CASE WHEN satRoutineIdx = ? THEN 0 ELSE satRoutineIdx END,
-                      sunRoutineIdx = CASE WHEN sunRoutineIdx = ? THEN 0 ELSE sunRoutineIdx END
-                  WHERE userId = ?;
-                  `;
-
-    const deleteRoutine = `
-                  UPDATE routine
-                  SET status = '1'
-                  WHERE routineIdx = ?
-                  `;
-
-    await Promise.all([
-        connection.query(deleteRoutineCalendar, [routineIdx, routineIdx, routineIdx, routineIdx, routineIdx, routineIdx, routineIdx, userId]),
-        connection.query(deleteRoutine, [routineIdx])
-    ]);
-
-    return ;
-}
   
 module.exports = {
-    selectRoutineCalendar,
-    selectRoutine,
-    deleteRoutine,
+    selectBeforeProcessDetail,
+    selectRoutineDetail,
 };
--- src/app/Process/ProcessProvider.js
+++ src/app/Process/ProcessProvider.js
@@ -1,22 +1,38 @@
 const { pool } = require("../../../config/database");
 const { logger } = require("../../../config/winston");
 
-const routineDao = require("./routineDao");
+const processDao = require("./processDao");
 
 // Provider: Read 비즈니스 로직 처리
 
-exports.retrieveRoutineCalendar = async function (userId) {
+// before 운동 /  세트, 무게, 횟수
+exports.retrieveBeforeProcessDetail = async function (routineDetailIdx) {
     const connection = await pool.getConnection(async (conn) => conn);
-    const routineCalendar = await routineDao.selectRoutineCalendar(connection, userId);
+    const BeforeProcessDetail = await processDao.selectBeforeProcessDetail(connection, routineDetailIdx);
     connection.release();
 
-    return routineCalendar;
+    return BeforeProcessDetail;
 };
 
-exports.retrieveRoutine = async function (curriIdx) {
+// 운동 중 / 세트, 무게, 횟수
+exports.retrieveProcessDetail = async function (routineIdx) {
     const connection = await pool.getConnection(async (conn) => conn);
-    const routine = await routineDao.selectRoutine(connection, curriIdx);
+    const routineDetail = await processDao.selectProcessDetail(connection, routineIdx);
+    connection.release();
+
+    return routineDetail;
+}
+
+// 시간, 총무게, 소요칼로리(db 추가)
+exports.retrieveProcessEnd = async function (userId) {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const processEnd = await processDao.selectMyCalendar(connection, userId);
     connection.release();
 
     return routine;
 };
+
+// gpt콜..
+exports.retrieveProcessEndDetail = async function (userId) {
+
+}
