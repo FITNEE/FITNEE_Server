0a46cd432b1b304a440166b29088eb359dd750ab
헥스/박해승 <2001phs92@gmail.com>
2023-08-11 00:09:15 +0900

    update server log

--- src/dev/serverExpress.js
+++ src/dev/serverExpress.js
@@ -39,7 +39,7 @@ module.exports = function() {
     });
 
     app.get('/cat-log', (req, res) => {
-        exec('tail -50 /home/ubuntu/git_clone/healthgpt_backend/log/index.log', (err, stdout, stderr) => {
+        exec('tail -50 /home/ubuntu/.forever/5WoI.log', (err, stdout, stderr) => {
             if (err) {
                 console.error(err);
                 return res.sendStatus(500);
