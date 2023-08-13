5eadc16be59c280638fd17376a4b341229157e72
헥스/박해승 <2001phs92@gmail.com>
2023-08-13 17:56:35 +0900

    update get  today routine time env

--- src/app/Routine/routineDao.js
+++ src/app/Routine/routineDao.js
@@ -166,10 +166,11 @@ async function selectTodayRoutine(connection, userId) {
     if (!existRoutineIdx) {
         responseToday.isToday = false;
         var offset = 1;
-        while (existRoutineIdx = existRoutineCalendar[`${weekEn[(exerciseDay.getDay()+offset)%7]}RoutineIdx`]) offset += 1;
-        exerciseDay.setDate(exerciseDay.getDate()+offset);
-        existRoutineIdx = existRoutineCalendar[`${weekEn[exerciseDay.getDay()]}RoutineIdx`];
+        while (!(existRoutineIdx = existRoutineCalendar[`${weekEn[(exerciseDay.getDay()+offset)%7]}RoutineIdx`])) offset += 1;
+        
+        exerciseDay.setDate(exerciseDay.getDate()+offset+1);
         responseToday.todayStrKo = toStringByMyFormatting(exerciseDay);
+        console.log(existRoutineIdx);
         console.log(exerciseDay);
         console.log(exerciseDay.getHours());
     };
