44053b97997716258da8074fbec5794562842245
헥스/박해승 <2001phs92@gmail.com>
2023-09-04 17:37:26 +0900

    update check

--- src/dev/log.js
+++ src/dev/log.js
@@ -2,3 +2,6 @@ ASDF
 ASDFASDFASDFASDFASDF
 ASDF
 ASDF
+
+//forever start -l /home/ubuntu/.forever/_yv3.log -a -c nodemon index.js
+//forever start -l /home/ubuntu/.forever/pZT5.log -a -c nodemon dev.index.js
