0e0540ef93d052dcd867ca950f472548864025c2
fbwougr121 <fbwogur121@naver.com>
2023-09-08 13:53:42 +0900

    promotion 사전예약 정보 바뀌면 status 바뀌게 쿼리 수정

--- src/app/Promotion/promotionController.js
+++ src/app/Promotion/promotionController.js
@@ -15,23 +15,23 @@ exports.postPromotion = async function (req, res) {
     const existCheckNum = await promotionProvider.returnExist(email, phoneNum);
     console.log(existCheckNum);
 
-    // if existCheckNum 1 - 이메일 and 번호 둘다 같은게 있으면 이미 사전예약 했습니다 띄우기
+    // existCheckNum 1 - 이메일 and 번호 둘다 같은게 있으면 이미 사전예약 했습니다 띄우기
     if (existCheckNum === 1) return res.send(errResponse(baseResponse.PROMOTION_ALREADY_ENTERED));
 
 
-    // if existCheckNum 2 - 이메일 동일한게 있다면 전화번호 덮어쓰기
+    // existCheckNum 2 - 이메일 동일한게 있다면 전화번호 덮어쓰기
     else if (existCheckNum === 2) {
         const responsePostPromotion = await promotionService.changePhoneNum(email, phoneNum);
         return res.send(response(baseResponse.SUCCESS, responsePostPromotion));
     }
     
-    // if existCheckNum 3 - 번호 동일한게 있다면 이메일 덮어쓰기
+    // existCheckNum 3 - 번호 동일한게 있다면 이메일 덮어쓰기
     else if (existCheckNum === 3) {
         const responsePostPromotion = await promotionService.changeEmail(email, phoneNum);
         return res.send(response(baseResponse.SUCCESS, responsePostPromotion));
     }
 
-    // if existCheckNum 4 - 동일한거 없는경우 -> db에 넣기
+    // existCheckNum 4 - 동일한거 없는경우 -> db에 넣기
     else {
         const responsePostPromotion = await promotionService.insertPromotion(email, phoneNum);
         return res.send(response(baseResponse.SUCCESS, responsePostPromotion));
