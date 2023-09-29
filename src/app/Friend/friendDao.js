960cbad131db78cc7652f49cb0dbe2816962725e
fbwougr121 <fbwogur121@naver.com>
2023-09-29 15:57:24 +0900

    friendDao searchFriend null삭제로 수정

--- src/app/Friend/friendDao.js
+++ src/app/Friend/friendDao.js
@@ -5,21 +5,30 @@ async function searchFriend(connection, userIdFromJWT) {
             CASE
                 WHEN fl.fromUserIdx = u.userIdx THEN u2.userNickname
                 WHEN fl.toUserIdx = u.userIdx THEN u1.userNickname
-        END AS friendNickname
+            END AS friendNickname
         FROM friendList AS fl
         LEFT JOIN User AS u ON u.userId = ?
         LEFT JOIN User AS u1 ON u1.userIdx = fl.fromUserIdx
         LEFT JOIN User AS u2 ON u2.userIdx = fl.toUserIdx
-        WHERE (u1.userIdx IS NOT NULL OR u2.userIdx IS NOT NULL) AND (u1.status=1 and u2.status=1);
+        WHERE (u1.userIdx IS NOT NULL OR u2.userIdx IS NOT NULL) AND (u1.status = 1 AND u2.status = 1) AND fl.status = '1';
     `;
     const [searchResultRows] = await connection.query(searchFriendQuery, [userIdFromJWT]);
-    const friendCount = searchResultRows.length;
+
+    // null 값을 필터링하여 새로운 배열을 생성합니다.
+    const filteredFriendList = searchResultRows
+        .filter((friend) => friend.friendNickname !== null)
+        .map((friend) => ({ friendNickname: friend.friendNickname }));
+
+    // 친구 목록의 갯수는 필터링된 배열의 길이입니다.
+    const friendCount = filteredFriendList.length;
 
     return {
         friendCount,
-        friendList: searchResultRows,
+        friendList: filteredFriendList,
     };
 }
+
+
 
 module.exports = {
     searchFriend,
