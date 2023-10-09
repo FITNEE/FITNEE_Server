02205c20fe9e1e566c0e4a2b79450a8c838847ab
fbwougr121 <fbwogur121@naver.com>
2023-10-10 06:37:33 +0900

    friend 오타 수정

--- src/app/Friend/friendRoute.js
+++ src/app/Friend/friendRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwtMiddleware = require('../../middleware/jwtMiddleware');
-const freind = require('./freindController');
+const friend = require('./friendController');
 
 /**
  * Freind API
@@ -9,30 +9,33 @@ const freind = require('./freindController');
  */
 
 // 1. 
-router.get('/', jwtMiddleware, freind.getFriendByIdx);
+router.get('/', jwtMiddleware, friend.getFriendByIdx);
 
 // 2. 
-router.post('/searchUser', freind.searchUser);
+router.post('/searchUser', friend.searchUser);
 
 // 3. 
-router.post('/addFriend', jwtMiddleware, freind.addFriend);
+router.post('/addFriend', jwtMiddleware, friend.addFriend);
 
 // 3.1
-router.get('/addFriendList', jwtMiddleware, freind.addFriendList);
+router.get('/addFriendList', jwtMiddleware, friend.addFriendList);
 
 // 3.2
-router.delete('/cancelAdd', jwtMiddleware, freind.cancelAdd);
+router.delete('/cancelAdd', jwtMiddleware, friend.cancelAdd);
 
 // 4.
-router.get('/getReceiveList', jwtMiddleware, freind.getReceiveList);
+router.get('/getReceiveList', jwtMiddleware, friend.getReceiveList);
 
 // 4.1
-router.put('/accept', jwtMiddleware, freind.acceptFriend);
+router.put('/accept', jwtMiddleware, friend.acceptFriend);
 
 // 4.2
-router.delete('/refuse', jwtMiddleware, freind.refuseFriend);
+router.delete('/refuse', jwtMiddleware, friend.refuseFriend);
 
 // 5
-router.delete('/delete', jwtMiddleware, freind.deleteFriend);
+router.delete('/delete', jwtMiddleware, friend.deleteFriend);
+
+// pushAlarm test
+//router.get('/push', friend.pushAlarm)
 
 module.exports = router;
