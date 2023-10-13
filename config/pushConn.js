f5d7b4bddaca7a82cbce0f3ba6d019168bdd23b7
fbwougr121 <fbwogur121@naver.com>
2023-10-13 17:02:12 +0900

    .

--- config/pushConn.js
+++ config/pushConn.js
@@ -1,19 +1,19 @@
-const adminAndroid = require('firebase-admin');
+// const adminAndroid = require('firebase-admin');
 
-let androidServiceAccount = require('./firebase-admin.json')
+// let androidServiceAccount = require('./firebase-admin.json')
 
-const admin = adminAndroid.initializeApp({
-	credential : adminAndroid.credential.cert(androidServiceAccount)
-    });
+// const admin = adminAndroid.initializeApp({
+// 	credential : adminAndroid.credential.cert(androidServiceAccount)
+//     });
     
-module.exports = admin;
-
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require('./firebase-admin.json')
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
 // module.exports = admin;
+
+// // var admin = require("firebase-admin");
+
+// // var serviceAccount = require('./firebase-admin.json')
+
+// // admin.initializeApp({
+// //   credential: admin.credential.cert(serviceAccount)
+// // });
+
+// // module.exports = admin;
